124700f6f9ec026f4b625209f3a1d3c6fd5b2d8d
tachi <hamdhanb8@gmail.com>
2025-07-29 10:18:09 +0400

    ex00

---
+++ CPP01/ex00/main.cpp
@@ -0,0 +1,11 @@
+#include "zombie.hpp"
+
+int main(){
+    Zombie* heap = newZombie("heap_zombie");
+    heap->announce();
+    delete heap;
+    randomChump("random_chump_zombie");
+    Zombie stack("stack_zombie");
+    stack.announce();
+    return 0;
+}
