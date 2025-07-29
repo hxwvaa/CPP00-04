6e5d3878377807af187fe73f8526c7c1e017611b
tachi <hamdhanb8@gmail.com>
2025-07-29 09:49:13 +0400

    01

---
+++ CPP01/ex00/zombie.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+class Zombie {
+    private:
+        std::string name;
+    public:
+        Zombie();
+        Zombie(std::string name);
+        ~Zombie();
+        void announce() const;
+}
