0caea9c660859bed80880ae89518b4310f8b8148
tachi <hamdhanb8@gmail.com>
2025-08-07 16:52:20 +0400

    ex01

---
+++ CPP03/ex01/main.cpp
@@ -0,0 +1,19 @@
+#include "ScavTrap.hpp"
+
+int main() {
+    ScavTrap scavtrap1("Me");
+    ScavTrap scavtrap2("You");
+
+    scavtrap1.attack(scavtrap2.getName());
+    scavtrap2.takeDamage(-15);
+    scavtrap2.beRepaired(3);
+
+    scavtrap1.attack(scavtrap2.getName());
+    scavtrap2.takeDamage(10);
+
+    scavtrap1.beRepaired(5);
+
+    scavtrap1.guardGate();
+
+    return 0;
+}
